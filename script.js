829ddab0b0a6f23e23f8223ae1e3435cd658214f
andkymon <100330163+andkymon@users.noreply.github.com>
2024-10-09 20:57:35 +0800

    Change grid square bg color to black on mouseover

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const container = document.querySelector("#container");
 
-
 for (i = 0; i < 256; i++) {
     const square = document.createElement("div");
 
+    square.className = "square";
     square.style.height = "6.25%";
     square.style.width = "6.25%";
-
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    });
     container.appendChild(square);
 };
+
+
 
